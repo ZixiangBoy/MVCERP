379c5139e5d7716fc1683b1cf8db6078a22472df
leo <leo.wei@hotmail.com>
2016-01-07 15:12:22 +0800

    leo

---
+++ MVCERP/Areas/System/Controllers/DictionaryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCERP.Areas.System.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        // GET: System/Dictionary
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
